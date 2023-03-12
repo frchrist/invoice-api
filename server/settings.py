6737a439ec56f85caa2cc415ef3509d0aa451074
christi@n <coding12online@gmail.com>
2023-03-12 02:29:35 +0000

    add postgresDB

--- server/settings.py
+++ server/settings.py
@@ -68,10 +68,21 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
+    "default" : {
+        "ENGINE" : "django.db.backends.postgresql_psycopg2",
+        "NAME" : "postgres",
+        "HOST" : "db.rekmwpkdltqqyhyiumob.supabase.co",
+        "PORT" : 5432,
+        "USER" : "postgres",
+        "PASSWORD" : os.environ.get("INVOICE_DB"),
+
     }
+
 }
 
 
