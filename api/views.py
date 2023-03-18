80060c8aeb6cd54140be0fae2bd9d36489d35075
christi@n <coding12online@gmail.com>
2023-03-18 09:26:31 +0000

    fix importing error

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework import status
 from rest_framework import Response
+from rest_framework.response import Response
 from rest_framework import renderers
 from .process import html_to_pdf
 
