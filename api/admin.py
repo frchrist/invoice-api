ce0f3bb98b56a98fa62578c77e542112a8f4e771
christi@n <coding12online@gmail.com>
2023-07-07 23:04:52 +0000

    adding client to admin section

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Product, Invoice, ProductInInvoice
+from .models import Product, Invoice, ProductInInvoice, Client
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Invoice)
+admin.site.register(Client)
+
 
 admin.site.register(ProductInInvoice)
 
